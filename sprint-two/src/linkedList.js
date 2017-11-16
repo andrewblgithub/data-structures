155be460a9044f792ddb9cfe036508759f1b0a3a
Andrew Lichtenstein <andrew@Andrews-MacBook-Pro.local>
2017-11-16 10:12:27 -0500

    finished linked lists

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,12 +4,31 @@ var LinkedList = function() {
   list.tail = null;
 
   list.addToTail = function(value) {
+    var newNode = Node(value);
+    if (list.head === null) {
+      list.head = newNode;
+      list.tail = newNode;
+    } else {
+      list.tail.next = newNode;
+      list.tail = newNode;
+    }
   };
 
   list.removeHead = function() {
+    var removed = list.head.value;
+    list.head = list.head.next;
+    return removed;
   };
 
   list.contains = function(target) {
+    var temp = list.head;
+    while (temp.value !== target) {
+      if (temp.next === null) { 
+        return false;
+      }
+      temp = temp.next;
+    }
+    return true;
   };
 
   return list;
