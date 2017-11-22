4ad3b33b8defb19b40a11a16b51840144a3c0cd9
Andrew Lichtenstein <andrew@Andrews-MBP.fios-router.home>
2017-11-21 22:41:34 -0500

    code cleanup

--- sprint-two/spec/linkedListSpec.js
+++ sprint-two/spec/linkedListSpec.js
@@ -51,7 +51,7 @@ describe('linkedList', function() {
     expect(linkedList.contains(4)).to.equal(false);
   });
 
-  if('should return length of list', function() {
+  it('should return length of list', function() {
     linkedList.addToTail(4);
     linkedList.addToTail(5);
     expect(linkedList.length).to.equal(2);    
