1f6d9dca4d65706a40c9e1f6a7976e537d009075
Andrew Lichtenstein <andrew@Andrews-MacBook-Pro.local>
2017-11-22 21:53:15 -0500

    added removeFromParent and traverse methods

--- sprint-two/spec/treeSpec.js
+++ sprint-two/spec/treeSpec.js
@@ -2,7 +2,7 @@ describe('tree', function() {
   var tree;
 
   beforeEach(function() {
-    tree = Tree();
+    tree = Tree(1);
   });
 
   it('should have methods named "addChild" and "contains", and a property named "value"', function() {
@@ -32,6 +32,11 @@ describe('tree', function() {
     expect(tree.children[0].children[0].value).to.equal(6);
   });
 
+  it('should return parent of child', function() {
+    tree.addChild(5);
+    expect(tree.children[0].parent).to.equal(tree);
+  });
+
   it('should correctly detect nested children', function() {
     tree.addChild(5);
     tree.addChild(6);
@@ -44,6 +49,24 @@ describe('tree', function() {
   it('should return the size of the tree', function() {
     tree.addChild(5);
     expect(tree.size).to.equal(2);
-  })
+  });
+
+  it('should remove link between node and parent if removeFromParent is called', function() {
+    tree.addChild(6);
+    var tempChild = tree.children[0];
+    tree.removeFromParent(6);
+    expect(tempChild.parent).to.equal(null);
+    expect(tree.children[0]).to.equal(undefined);
+  });
+
+  it('should execute a callback on every tree member when traverse is called', function() {
+    var array = [];
+    var func = function(value) { array.push(value); };
+    tree.addChild(2);
+    tree.addChild(3);
+    tree.children[1].addChild(4);
+    tree.traverse(func);
+    expect(array).to.eql([1, 2, 3, 4]);
+  });
 
 });
--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -2,8 +2,7 @@ var Tree = function(value) {
   var newTree = {};
   newTree.value = value;
   newTree.size = 1;
-
-  // your code here
+  newTree.parent = null;
   newTree.children = [];
   _.extend(newTree, treeMethods);
 
@@ -14,28 +13,56 @@ var treeMethods = {};
 
 treeMethods.addChild = function(value) {
   var newChild = Tree(value);
+  newChild.parent = this;
   this.children.push(newChild);
   this.size++;
 };
 
 treeMethods.contains = function(target) {
   var passed = [];
-  function check(node) {
+  var checkNodes = function(node) {
     if (node.value === target) {
       passed.push(node);
     }
     for (var i = 0; i < node.children.length; i++) {
-      check(node.children[i]);
+      checkNodes(node.children[i]);
     }
-  }
-  check(this);
+  };
+  checkNodes(this);
   return passed.length > 0 ? true : false;
 };
 
+treeMethods.removeFromParent = function(target) {
+  var checkNodesRFP = function(node) {
+    if (node.value === target) {
+      for (var i = 0; i < node.parent.children.length; i++) {
+        if (node.parent.children[i].value === target) {
+          node.parent.children.splice(i, 1);
+        }
+      }
+      node.parent = null;
+    }
+    for (var i = 0; i < node.children.length; i++) {
+      checkNodesRFP(node.children[i]);
+    }
+  };
+  checkNodesRFP(this);
+};
 
+treeMethods.traverse = function(cb) {
+  var traverseNodes = function(node) {
+    cb(node.value);
+    for (var i = 0; i < node.children.length; i++) {
+      traverseNodes(node.children[i]);
+    }
+  };
+  traverseNodes(this);
+};
 
 /*
  * Complexity: What is the time complexity of the above functions?
  * addChild - O(1)
  * contains - O(n)
+ * removeFromParent - 0(n)
+ * traverse - O(n)
  */
